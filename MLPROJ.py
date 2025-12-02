7890cb49f14d784fb8436858258e2cabadcb04e5
Sam Kramer <119812361+skramer9@users.noreply.github.com>
2025-12-01 20:56:27 -0600

    Added pseudocode

---
+++ MLPROJ.py
@@ -0,0 +1,14 @@
+#load all models
+#data structure for storing game board
+
+#play each model against each other (do one where each goes first?)
+    #before starting, clear game board and select two models
+    #general loop:
+        #determine whose turn it is
+        #determine what moves are possible
+        #predict chance of winning for each possible moves
+        #make the move with highest chance of winning
+        #check if the games ends
+            #if game ends, record who won (maybe record whether the first player won?)
+            #brake loop iteration
+        #change whose turn it is
